8d5b523a26477765438603adf0d54b3cd062ebf6
164c <morotomi.hiroshi@gmail.com>
2018-08-02 18:52:58 +0900

    first commit

---
+++ src/test/java/hello/model/UserTest.java
@@ -0,0 +1,21 @@
+package hello.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    @DisplayName("╯°□°）╯")
+    void getNameReturnsName() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void getNameReturnsName2() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
